da62a7e7fe47d056701090b94117396a329dfcf1
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-28 10:27:05 +0200

    Set up backend link to frontend

---
+++ resources/js/Pages/Contact.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Contact us
+        <inertia-link href="/hello">Go to the home page</inertia-link>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ resources/js/Pages/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Hi there, i am the hello page
+
+            <div v-for="(message, index) in messages" :key="index">
+                {{ message.text }}
+            </div>
+
+        <inertia-link href="/contact">Go to the contact page</inertia-link>
+    </div>
+ 
+</template>
+
+<script>
+export default {
+    props: {
+        messages: Array
+    }
+}
+</script>
